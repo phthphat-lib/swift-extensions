2c33e1a9f806764e88f44b44ca2a7e8eb8516c48
Thanh Phát <phthphat@gmail.com>
2020-02-27 14:40:00 +0700

    Update example project

--- README.md
+++ README.md
@@ -2,3 +2,31 @@
 
 - Like the title, it's extension collected by phthphat
 - Thanks to Vinova team, Brian Voong 
+
+SampleView
+![Sample view](./Image/sampleView.png)
+```swift
+vstack(
+    hstack(
+        UIImageView(backgroundColor: .red)
+            .with(width: 50)
+            .with(cornerRadius: 25),
+        UILabel(text: "David XXX"),
+        UIButton(title: "Press me", titleColor: .systemBlue, target: self, action: #selector(onTapBtn)),
+        spacing: 10
+    ).with(margins: .init(top: 5, left: 5, bottom: 5, right: 5)).with(height: 60),
+    UIView(backgroundColor: .systemGray),
+    hstack(
+        UILabel(text: "The quick brown fox \n jump over the \n lazy dog", numberOfLines: 0),
+        UILabel(text: "Hey, what're you gonna do today", numberOfLines: 0),
+        spacing: 10
+    ),
+    hstack(
+        UIButton(title: "Hello", titleColor: .green),
+        UIButton(title: "World", titleColor: .red),
+        UIButton(title: "!", titleColor: .brown),
+        distribution: .fillEqually
+    ),
+    spacing: 10
+)
+```
--- Sources/Extensions/View/UIView/UIView+Shadow.swift
+++ Sources/Extensions/View/UIView/UIView+Shadow.swift
@@ -13,17 +13,22 @@ public extension UIView {
     func endOfView() -> CGFloat {
         return (self.frame.origin.y+self.frame.size.height)
     }
-    
-    func setShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+    @discardableResult
+    func withShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, cornerRadius: CGFloat? = nil, scale: Bool = true) -> UIView {
         layer.masksToBounds = false
         layer.shadowColor = color.cgColor
         layer.shadowOpacity = opacity
         layer.shadowOffset = offSet
         layer.shadowRadius = radius
+        if let cornerRad = cornerRadius {
+            layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: cornerRad).cgPath
+        } else {
+            layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        }
         
-        layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
         layer.shouldRasterize = true
         layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+        return self
     }
     
     func addShadow(color: UIColor, alpha: Float, x: CGFloat, y: CGFloat, blur: CGFloat) {
